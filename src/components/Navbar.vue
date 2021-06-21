f246c1663684e6c2608bb0266fd2af2d2b055614
mccrush <mccrush2027@gmail.com>
2021-06-21 13:27:08 +0700

    Settings nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,29 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-light bg-light">
-  <div class="container-fluid">
-    <ul class="navbar-nav me-auto">
+    <div class="container-fluid">
+      <ul class="navbar-nav me-auto">
         <li class="nav-item">
-           <router-link to="/" class="nav-link">Index</router-link>
+          <router-link to="/" class="nav-link">Index</router-link>
+        </li>
+        <!-- <li class="nav-item">
+          <router-link to="/razdel" class="nav-link">Razdel</router-link>
         </li>
         <li class="nav-item">
-           <router-link to="/razdel" class="nav-link">Razdel</router-link>
+          <router-link to="/razdel/page" class="nav-link">Page</router-link>
         </li>
         <li class="nav-item">
-           <router-link to="/razdel/page" class="nav-link">Page</router-link>
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li> -->
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">Login</router-link>
         </li>
         <li class="nav-item">
-           <router-link to="/about" class="nav-link">About</router-link>
+          <router-link to="/admin" class="nav-link">Admin</router-link>
         </li>
         <li class="nav-item">
-           <router-link to="/login" class="nav-link">Login</router-link>
+          <router-link to="/404" class="nav-link">404</router-link>
         </li>
-        <li class="nav-item">
-           <router-link to="/admin" class="nav-link">Admin</router-link>
-        </li>
-        <li class="nav-item">
-           <router-link to="/404" class="nav-link">404</router-link>
-        </li>
-  </div>
+      </ul>
+    </div>
   </nav>
 </template>
